a6d7152e3fe94efce5561bc61b3b5d39f09bf59d
EhfazAdnan <ehfaz15-167@diu.edu.bd>
2019-12-06 00:55:47 +0600

    Model factory and seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\User;
+use App\Category;
+use App\Product;
+use App\Transaction;
+use App\Seller;
 use Faker\Generator as Faker;
 
 /*
@@ -13,11 +18,45 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
+
+        'verified' => $verified = $faker->randomElement([User::VERIFIED_USER, User::UNVERIFIED_USER]),
+        'verification_token' => $verified = User::VERIFIED_USER ? null : User::generateVerificationCode(),
+        'admin' => $verified = $faker->randomElement([User::ADMIN_USER, User::REGULAR_USER]),
+    ];
+});
+
+$factory->define(Category::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+    ];
+});
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+        'quantity' => $faker->numberBetween(1, 10),
+        'status' => $faker->randomElement([Product::AVAILABLE_PRODUCT, Product::UNAVAILABLE_PRODUCT]),
+        'image' => $faker->randomElement(['1.jpg','2.jpg','3.jpg']),
+        'seller_id' => User::all()->random()->id,
+    ];
+});
+
+$factory->define(Transaction::class, function (Faker $faker) {
+
+    $seller = Seller::has('products')->get()->random();
+    $buyer = User::all()->except($seller->id)->random();
+
+    return [
+        'quantity' => $faker->numberBetween(1,3),
+        'buyer_id' => $buyer->id,
+        'product_id' => $seller->products->random()->id,
     ];
 });
